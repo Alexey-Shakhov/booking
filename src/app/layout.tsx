60a3c7937b5863c251b8fc017f391ac6b649b26c
Alexey Shakhov <alexeymshakhov@gmail.com>
2026-05-30 18:41:58 +0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from '@/app/header'
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin", "cyrillic"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin", "cyrillic"],
+});
+
+export const metadata: Metadata = {
+    title: "Бронирование кабинетов",
+    description: "Приложение для бронирования кабинетов",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html
+            lang="en"
+            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+        >
+            <body className="min-h-full flex flex-col">{children}
+                <Header />
+            </body>
+        </html>
+    );
+}
